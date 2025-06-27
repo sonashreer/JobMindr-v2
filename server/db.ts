3fad272b73bb080a7fe361ff5b3c009ae132ea9f
Sonashree Roy <68667367+sonashreer@users.noreply.github.com>
2025-06-26 21:40:57 -0400

    Updated

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,16 @@
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set");
 }
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // required for Render Postgres
+  ssl: { rejectUnauthorized: false } // Required for Render PostgreSQL
 });
+
 export const db = drizzle(pool, { schema });
